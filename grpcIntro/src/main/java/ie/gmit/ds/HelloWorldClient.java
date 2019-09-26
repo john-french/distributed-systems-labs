4f30689aaac2ce9f8396853b7e20f0ff1eac6e7b
John French <frenche@gmail.com>
2019-09-27 00:21:26 +0100

    Initial grpcIntro commit

---
+++ grpcIntro/src/main/java/ie/gmit/ds/HelloWorldClient.java
@@ -0,0 +1,52 @@
+package ie.gmit.ds;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorldClient {
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    public HelloWorldClient(String host, int port) {
+        this.channel = ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        try {
+            client.greet("world");
+        } finally {
+            client.shutdown();
+        }
+    }
+}
